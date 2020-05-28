74f146814874e93df125cd84a2f344bacd3528c9
XiaoqianDing <dxq3442@gmail.com>
2020-05-28 16:36:46 +1000

    initiate project

---
+++ src/index/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store';
+import './index.css';
+import App from './App';
+import 'normalize.css/normalize.css';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
